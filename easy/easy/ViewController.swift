bc66c08c444798b83fa0fdd9ebead78dfc0e546b
772312181 <wandile.banda2002@gmail.com>
2025-01-26 23:48:38 -0500

    Pre-work submission (work in progress)

---
+++ easy/easy/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  easy
+//
+//  Created by Wandile Banda on 1/26/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet weak var changeBackgroundColor: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ easy/easyTests/easyTests.swift
@@ -0,0 +1,17 @@
+//
+//  easyTests.swift
+//  easyTests
+//
+//  Created by Wandile Banda on 1/26/25.
+//
+
+import Testing
+@testable import easy
+
+struct easyTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
